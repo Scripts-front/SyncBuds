9fdf2d039149ff91f5637f980607ba141017ad7e
biel.lil <biel1313biel@gmail.com>
2026-03-26 22:21:24 -0300

    🔧 chore(05-02): add SyncBudsWidget extension target

---
+++ SyncBudsWidget/SyncBudsWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SyncBudsWidgetBundle.swift
+//  SyncBudsWidget
+//
+//  Created by Jose Gabiel on 2026/03/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SyncBudsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SyncBudsWidget()
+    }
+}
